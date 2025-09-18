9a5430520cb8bfca53991c870e94c75c2dde0143
Joe Anderson <joe@mousetrapped.co.uk>
2025-09-18 08:15:32 +0100

    Fix insertText

--- packages/core/src/applyToYjs/text/insertText.ts
+++ packages/core/src/applyToYjs/text/insertText.ts
@@ -2,26 +2,31 @@ import { InsertTextOperation, Node, Text } from 'slate';
 import type * as Y from 'yjs';
 import { getYTarget } from '../../utils/location';
 import { getProperties } from '../../utils/slate';
+import { emptyTextAttribute } from '../../utils/yjs';
 
 export function insertText(
   sharedRoot: Y.XmlText,
   slateRoot: Node,
   op: InsertTextOperation
 ): void {
-  const { yParent: target, textRange } = getYTarget(
-    sharedRoot,
-    slateRoot,
-    op.path
-  );
+  const {
+    yParent: target,
+    textRange,
+    slateTarget,
+    targetDelta,
+  } = getYTarget(sharedRoot, slateRoot, op.path);
 
-  const targetNode = Node.get(slateRoot, op.path);
-  if (!Text.isText(targetNode)) {
+  if (!Text.isText(slateTarget)) {
     throw new Error('Cannot insert text into non-text node');
+  }
+
+  if (targetDelta[0].attributes?.[emptyTextAttribute]) {
+    target.delete(textRange.start, 1);
   }
 
   target.insert(
     textRange.start + op.offset,
     op.text,
-    getProperties(targetNode)
+    getProperties(slateTarget)
   );
 }
--- packages/core/test/collaboration/insertText/inEmptyTextNode.tsx
+++ packages/core/test/collaboration/insertText/inEmptyTextNode.tsx
@@ -1,15 +1,12 @@
 /** @jsx jsx */
-import { Editor } from 'slate';
+import { Editor, Transforms } from 'slate';
 import { jsx } from '../../../../../support/jsx';
 
 export const input = (
   <editor>
     <unstyled>
       <text>one</text>
-      <text bold>
-        <cursor />
-      </text>
-      <text>three</text>
+      <text italic>three</text>
     </unstyled>
   </editor>
 );
@@ -18,15 +15,15 @@ export const expected = (
   <editor>
     <unstyled>
       <text>one</text>
-      <text bold>
-        two
-        <cursor />
-      </text>
-      <text>three</text>
+      <text bold>two</text>
+      <text italic>three</text>
     </unstyled>
   </editor>
 );
 
 export function run(editor: Editor) {
-  editor.insertText('two');
+  Editor.withoutNormalizing(editor, () => {
+    Transforms.insertNodes(editor, <text bold />, { at: [0, 1] });
+    Transforms.insertText(editor, 'two', { at: [0, 1] });
+  });
 }
